6ab143526c5c1f90bfb1d0aea3d8f380c06ad1e4
dingdang-beiyang <1205372258@qq.com>
2022-09-10 13:52:28 +0800

    simulation v0.3 add the draw picture function

--- simulation.py
+++ simulation.py
@@ -1,9 +1,14 @@
+# -*- coding: utf-8 -*-
 import queue
 import threading
 import time
 
+import matplotlib.pyplot as plt
+
+plt.rcParams['font.sans-serif'] = ['SimHei']  # 显示汉字
+
 queueLock = threading.Lock()
-reqQueue = queue.Queue(100)
+reqQueue = queue.Queue(1000)
 
 workers = []
 over_flag = False
@@ -35,31 +40,25 @@ class reqThread(threading.Thread):
         self.time_stamp = time.time()
 
     def run(self):
-        print("req到达：" + self.name + "  ")
-
-
-def create_workers(work_num, batch_size, duration):
-    for i in range(work_num):
-        new_worker = worker(i, batch_size, duration)
-        workers.append(new_worker)
-        new_worker.start()
+        print("req到达：%s" % self.name)
+        # 查看时间戳
+        # print("req到达：%s , time: %f  " % (self.name, self.time_stamp))
 
 
 class master(threading.Thread):
-    def __init__(self):
+    def __init__(self, req_num):
         threading.Thread.__init__(self)
+        self.req_num = req_num
 
     def run(self):
-        while True:
+        while self.req_num != 0:
             for each_worker in workers:
-                if self.detection():
+                if detection():
                     queueLock.acquire()
                     each_worker.collect()
+                    self.req_num -= 1
 
-    def detection(self):
-        while reqQueue.empty():
-            continue
-        return True
+        pic()
 
 
 class worker(threading.Thread):
@@ -69,6 +68,7 @@ class worker(threading.Thread):
         self.batch_size = batch_size
         self.duration = duration
         self.task_queue = queue.Queue(batch_size)
+        self.wcl_list = []
 
     def collect(self):
         self.task_queue.put(self.collect_req())
@@ -89,21 +89,61 @@ class worker(threading.Thread):
         first_req_time = self.task_queue.get().time_stamp
         self.task_queue.queue.clear()
 
-        print("worker " + str(self.workerID) + " work...")
+        print("worker %s work..." % self.workerID)
         time.sleep(self.duration)
 
         done_time = time.time()
-        print("worker " + str(self.workerID) + " done...and WCL is " + str(done_time - first_req_time))
+        wcl_time = done_time - first_req_time
+        self.wcl_list.append(wcl_time)
+        print("worker %s done...and WCL is %s " % (self.workerID, wcl_time))
 
     def run(self):
-        print("worker" + str(self.workerID) + "启动！")
+        print("worker %s 启动！" % self.workerID)
+
+
+def create_workers(work_num, batch_size, duration):
+    for i in range(work_num):
+        new_worker = worker(i, batch_size, duration)
+        workers.append(new_worker)
+        new_worker.start()
+
+
+def detection():
+    while reqQueue.empty():
+        continue
+    return True
+
+
+def pic():
+    pic_worker = workers[0]
+    pic_x = range(1, len(pic_worker.wcl_list) + 1)
+
+    color_list = ['r', 'salmon', 'tomato', 'lightsalmon', 'firebrick', 'tan']
+    marker_list = ['o', 'v', 's', 'p', 'h', 'd']
+    index = 0
+
+    for done_worker in workers:
+        plt.plot(pic_x,
+                 done_worker.wcl_list,
+                 color=color_list[index],
+                 marker=marker_list[index],
+                 linestyle='-',
+                 label=done_worker.workerID
+                 )
+        index += 1
+
+    plt.legend()  # 显示图例
+    plt.xticks(pic_x, pic_x, rotation=45)
+    plt.xlabel("运行次数")  # X轴标签
+    plt.ylabel("Worst Case Latency")  # Y轴标签
+    plt.show()
 
 
 if __name__ == '__main__':
-    create_workers(2, 2, 2)
+    create_workers(6, 10, 1)
 
-    Client = client(2, 50)
+    Client = client(10, 1200)
     Client.start()
 
-    Master = master()
+    Master = master(1200)
     Master.start()
